45c979bc198d7b95354e2e3c83590d2819eb9661
zhouning <2267431887@qq.com>
2019-09-02 14:35:50 +0800

    完善注释

--- src/main/java/com/gysoft/jdbc/multi/GyJdbcRoutingDataSource.java
+++ src/main/java/com/gysoft/jdbc/multi/GyJdbcRoutingDataSource.java
@@ -8,7 +8,8 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 public class GyJdbcRoutingDataSource extends AbstractRoutingDataSource {
     /**
      * 默认的数据源key;如果您的dao没有调用任何bind***()方法
-     * 则该值会很有用
+     * 或者在对应的方法上使用了@BindPoint("slave")注解
+     * 则该值会很有用,他会帮我们应用一个全局的数据源key
      */
     private String defaultLookUpKey;
 
