acd097d85589cc4b5766071cb77cc18265c69cb5
unknown <2267431887@qq.com>
2022-10-18 10:17:30 +0800

    添加详细注释

--- src/main/java/com/gysoft/jdbc/bean/SQLInterceptor.java
+++ src/main/java/com/gysoft/jdbc/bean/SQLInterceptor.java
@@ -1,26 +1,33 @@
 package com.gysoft.jdbc.bean;
 
 /**
+ * 用于在构建sql、参数数组钱执行相应逻辑，
+ * 可用于添加统一的条件、强制更新时间戳、记录sql等,
+ *
  * @author zhouning
  */
 public interface SQLInterceptor {
     /**
-     * 在真正构建sql和参数之前执行
+     * 在真正构建sql和参数之前执行，该方法会影响最终的sql和参数
+     * 对于EntityDao无Criteria、Sql参数的方法第二个参数为null，需要自行非空判断
+     *
      * @param criteria 组装撑的sql对象
      * @throws Exception sql错误抛出异常
      * @author ZhouNing
      * @date 2022/10/17 16:24
      **/
-    void beforeBuild(SQLType sqlType, AbstractCriteria criteria)throws Exception;
+    void beforeBuild(SQLType sqlType, AbstractCriteria criteria) throws Exception;
+
     /**
-     * 成功构建sql和参数后执行
-     * @param sql 真正的sql字符串
+     * 成功构建sql和参数后执行，该方法执行不能够影响sql
+     *
+     * @param sql  真正的sql字符串
      * @param args sql入参数组
      * @throws Exception sql错误抛出异常
      * @author ZhouNing
      * @date 2022/10/17 16:28
      **/
-    void afterBuild(String sql,Object[] args)throws Exception;
+    void afterBuild(String sql, Object[] args) throws Exception;
 
 
 }
